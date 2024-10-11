8352e5156e62035367c65faf68dd11d9c51c9e4a
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-11 13:38:49 +0000

    push ConvertionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,79 @@
+let score = "Arjun"
+
+//  console.log(typeof score); // String
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // Number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Arjun"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn); // boolean
+
+// 1 => true; 0 =>false
+// " " => false
+// Arjun => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber); // stirng
+
+
+
+
+//  ################# Operation #################### //
+
+let value = 3
+let negValue = -value
+console.log(negValue); // -3
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Arjun"
+let str2 = "Kumar"
+
+let str3 = str1 + str2
+console.log(str3); // ArjunKumar
+
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // 101
+
+
+
+
+// MDN Docs Link
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
