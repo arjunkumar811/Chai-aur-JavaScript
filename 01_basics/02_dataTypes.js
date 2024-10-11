73cd5106ba95cc32c1dd9842c52a02b89ed4d65d
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-11 13:22:46 +0000

    Add dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use stict"; // treat all js code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3
++
+3); // code readability should be high
+
+console.log("Arjun Kumar");
+
+let name = "Arjun"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true, false
+// null => empty var
+// undefined =>
+// symbol => unique
+
+
+
+// object
+ 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
