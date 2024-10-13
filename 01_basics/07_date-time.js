bc51c602fecfea6411d848091ebd07eded0ea007
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-13 15:42:50 +0000

    myDate

---
+++ 01_basics/07_date-time.js
@@ -0,0 +1,7 @@
+// dates
+
+let myDate = new Date()
+console.log(myDate); // 2024-10-13T15:35:08.010Z
+console.log(myDate.toString()); // Sun Oct 13 2024 15:35:08 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toLocaleString()); // 10/13/2024, 3:37:44 PM
+console.log(typeof myDate); // object
