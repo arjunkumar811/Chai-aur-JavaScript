17d1e142afe08c41c608cca1eab9b832169e9e40
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-13 16:59:34 +0000

    more Methord

--- 02_basics/01_arryas.js
+++ 02_basics/01_arryas.js
@@ -13,5 +13,15 @@ myArr.push(6)
 myArr.pop()
 myArr.push(7)
 
+myArr.unshift(9)
+myArr.shift()
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log( newArr);
 
 
