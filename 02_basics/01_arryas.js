37dbdcba64969466357163942424876cc964a0a7
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-13 16:57:19 +0000

    Added arr methords

---
+++ 02_basics/01_arryas.js
@@ -0,0 +1,17 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["my", "father"]
+
+const  myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr2); // [ 1, 2, 3, 4 ]
+
+
+// Array methods
+
+myArr.push(6)
+myArr.pop()
+myArr.push(7)
+
+
+
