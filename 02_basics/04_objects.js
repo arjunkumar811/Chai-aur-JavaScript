8842301c086c2c79ba0abadf84cc058ddb5bec85
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-13 19:44:59 +0000

    Code to join the objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -7,3 +7,26 @@ tinderUser.name = "Arjun"
 tinderUser.isLoggedIn = false
 
 console.log(tinderUser);  // { id: '123123asd', name: 'Arjun', isLoggedIn: false }
+
+
+const regularUser = {
+    email: "arjun@gmail.com",
+    fullname: {
+        username: {
+            firstName: "Arjun",
+            lastName: "Kumar"
+        }
+    }
+}
+
+console.log(regularUser.fullname.username.lastName); // Kumar
+
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+const obj4 = {...obj1, ...obj2}
+console.log(obj4);  // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
