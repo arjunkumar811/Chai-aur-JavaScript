afb5095b9bd41b85258c9b1bb3e719b0637aa063
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-13 19:36:19 +0000

    Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123123asd"
+tinderUser.name = "Arjun"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);  // { id: '123123asd', name: 'Arjun', isLoggedIn: false }
