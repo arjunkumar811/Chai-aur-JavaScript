cd6c5c5a7773679a3dace0dabcf8c02910095672
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-13 17:12:23 +0000

    push Code

--- 02_basics/01_arryas.js
+++ 02_basics/01_arryas.js
@@ -16,12 +16,43 @@ myArr.push(7)
 myArr.unshift(9)
 myArr.shift()
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(3));
+console.log(myArr.includes(9)); // false
+console.log(myArr.indexOf(3));  // 3
 
 const newArr = myArr.join()
 
-console.log(myArr);
+// console.log(myArr); // [
+//     0, 1, 2, 3,
+//     4, 5, 7
+//   ]
 console.log( newArr);
+
+// slice, splice
+
+// console.log("A ", myArr); // A  [
+//     0, 1, 2, 3,
+//     4, 5, 7
+//   ]
+
+const my1 = myArr.slice(1, 3)
+
+console.log(my1); // [ 1, 2 ]
+
+console.log("B", myArr);
+
+
+const my2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+// console.log(my2); // C  [ 0, 4, 5, 7 ]
+// [ 1, 2, 3 ]
+
+// The splice methord manupulate the array
+
+
+
+
+
+
+
 
 
