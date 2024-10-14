e78c3d543187f5fbd5d016c1268e7c6cc8adcbee
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-14 11:47:07 +0000

    myNewArray

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -66,3 +66,13 @@ handleObject({
     username: "Arjun",
     followers: 99
 });
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000])); // 400
