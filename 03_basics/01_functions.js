e39152981bd8aef5f0501e5c72e1596f03eae639
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-14 11:16:14 +0000

    Function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,11 @@
+// function MyName() {
+//     console.log("A");  // A
+//     console.log("R");  // R
+//     console.log("J");  // J
+//     console.log("U");  // U
+//     console.log("N");  // N
+// }
+
+// MyName()
+
+
