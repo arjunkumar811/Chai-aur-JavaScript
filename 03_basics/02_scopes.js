fa9c73ebdb77cf23c14e48f4a5aa2e7c15b00db5
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-14 12:04:05 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,28 @@
-// var c = 300
+// // var c = 300
 
-let a = 300
-if (true) {
-    let a = 10
-   // const b = 20
-    console.log("INNER: ", a); // INNER:  10 Inner Scope
+// let a = 300
+// if (true) {
+//     let a = 10
+//    // const b = 20
+//     console.log("INNER: ", a); // INNER:  10 Inner Scope
+// }
+
+// console.log(a); // 300
+// //console.log(b); // error
+// console.log(c); // 300
+
+function one() {
+    const username = "kumararjun"
+
+    function two() {
+        const website = "x.com"
+        console.log(username); // kumararjun
+        console.log(website); // x.com
+    
+    }  
+    two()
 }
 
-console.log(a); // 300
-//console.log(b); // error
-console.log(c); // 300
+one()
 
 
