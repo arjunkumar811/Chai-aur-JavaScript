661f769a50a1ffbb6f002fd596ecf3c12c92fbf2
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-14 12:20:10 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,24 @@
+const user = {
+    username: "Arjun",
+    password: 123123,
+
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+ user.welcomeMessage()
+ user.username = "sam"
+ user.welcomeMessage()
+
+ console.log(this);
+
+ function chai(){
+     let username = "hitesh"
+     console.log(this.username);
+ }
+
+chai()
