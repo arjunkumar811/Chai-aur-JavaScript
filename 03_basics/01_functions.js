598f0f0446ebf5df30c6104c4b58e7c665628615
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-14 11:26:54 +0000

    loginUserMessage

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,4 +8,28 @@
 
 // MyName()
 
+// function sum(n1, n2) {
+//     console.log(n1 + n2);
+// }
+
+// sum(1 ,2) // 3
+
+
+// function sum1(n1, n2) {
+// return n1 + n2;
+// }
+
+// sum1(2, 3)
+// console.log(sum1(2, 3)); // 5
+
+
+function loginUserMessage(username = "arjun") {
+    if(!username) {
+        console.log("Please enter a username");
+       return       
+    }
+    return `${username} just logged in`
+}
+ 
+console.log(loginUserMessage("Arjun")); // Arjun just logged in
 
