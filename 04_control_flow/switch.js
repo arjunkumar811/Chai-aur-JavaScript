1c49f636d8603cdacce9295bc67875bf78a92348
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-15 17:21:08 +0000

    Switch

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -7,3 +7,28 @@
 //     default:
 //         break;
 // }
+
+const month = "October"
+
+switch (month) {
+    case "january":
+    console.log("January");
+    break;
+
+    case "Feb":
+    console.log("Feb");
+    break;
+
+    case "March":
+    console.log("March");
+    break;
+
+    case "October":
+    console.log("October"); // October
+    break;
+
+
+    default:
+        console.log("default case match");
+        break;
+}
