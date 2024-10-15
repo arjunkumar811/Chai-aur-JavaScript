a5dd27235ca6d7db5e0022bb276dd2eb7c9bb518
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-15 17:31:17 +0000

    truthy values

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -8,5 +8,11 @@ if (userEmail) {
 
 
 // falsy values
-
 // false, 0, -0, BigInt on, "", null, undefinde, New
+
+// truthy values
+// "0", "false", " ", [], {}, function(){}
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
