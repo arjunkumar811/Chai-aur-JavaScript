f21876a470ca073eb5c0f8ad13570c0dc7246760
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-15 16:50:46 +0000

    Function

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+// if 
+const isUSerloggedIn = true
+const temperature = 41
+
+if ( temperature === 40 ){
+    console.log("less than 50");
+}  else {
+    console.log("temperature is greater than 50"); // temperature is greater than 50
+}
+
