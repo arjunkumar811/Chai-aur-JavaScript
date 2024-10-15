ec5c742a4222469ee9387f8f7f430f4205cc60d9
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-15 17:41:59 +0000

    Truthy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -35,3 +35,13 @@ val1 = null ?? 10 ?? 20
 
 
 console.log(val1); // 10
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+// more than 80
