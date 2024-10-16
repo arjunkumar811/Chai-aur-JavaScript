8ef1d051ced4dfa69cf6a678c35694b56d9f97b2
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-16 16:56:10 +0000

    Continue

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -32,10 +32,22 @@ for (let index = 0; index < myArray.length; index++) {
 
 // break and continue 
 
+// for (let i=1; i<=20; i++) {
+//     if(i == 5) {
+//         console.log(`Detected 5`); // Detected 5
+//        break
+//     }
+//     console.log(`value is : ${i}`);
+// }
+
+
+// continue
+
 for (let i=1; i<=20; i++) {
-    if(i == 5) {
+    if (i == 5) {
         console.log(`Detected 5`); // Detected 5
-       break
+        continue
     }
-    console.log(`value is : ${i}`);
+    console.log(`Value of i is ${i}`);
+    
 }
