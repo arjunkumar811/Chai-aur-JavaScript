69af2a821c37af31b27ac8dbde8f4edb4846aa67
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-16 16:47:07 +0000

    Loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,23 +1,27 @@
-// for 
+// // for 
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if(element == 5) { 
-        console.log("5 is best number"); // 5 is best number  
-    }
-    console.log(element); // 0 1 2 3 4 5 is best number 6 7 8 9 10
-}
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5) { 
+//         console.log("5 is best number"); // 5 is best number  
+//     }
+//     console.log(element); // 0 1 2 3 4 5 is best number 6 7 8 9 10
+// }
 
-// console.log(element); // Out of scope
+// // console.log(element); // Out of scope
 
 
-for (let i=1; i<=10; i++) {
-    console.log(`Outer loop value : ${i}`);
+// for (let i=1; i<=10; i++) {
+//     console.log(`Outer loop value : ${i}`);
 
-    for(let j=1; j<=10; j++) {
-    console.log(`Inner loop value : ${j}`);
-      console.log(i + '*' + j + ' = ' + i*j);
+//     for(let j=1; j<=10; j++) {
+//     console.log(`Inner loop value : ${j}`);
+//       console.log(i + '*' + j + ' = ' + i*j);
       
-    }
-}
+//     }
+// }
  
+
+let myArray = ["yash", "druve", "rokey"]
+console.log(myArray.length); // 3
+
