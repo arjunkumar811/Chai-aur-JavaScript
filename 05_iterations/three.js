2cdaf9f95676e7642a94e4e2fd4f53dc38b60046
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-17 09:33:04 +0000

    Added MAP

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -9,8 +9,17 @@
     
 // }
 
-const greeting = "Hello Arjun!"
-for (const greet of greeting) {
-    console.log(`Ecah char is ${greet}`);
-    
-}
+// const greeting = "Hello Arjun!"
+// for (const greet of greeting) {
+//     console.log(`Ecah char is ${greet}`);
+// }
+
+// Maps 
+const map = new Map() 
+map.set('IN', "india")
+map.set('USA', "United Sates of America")
+map.set('FR', "France")
+
+
+console.log(map);
+
