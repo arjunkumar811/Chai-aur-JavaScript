4a815c5c1a59ae2268ea7b3580e59ccd8b731e27
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-17 09:39:42 +0000

    Threee.js

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,25 +1,40 @@
-// for of
+for of
 
-// ["", "", ""]
-// [{}, {}, {}]
+["", "", ""]
+[{}, {}, {}]
 
-// const arr = [1, 3, ,4 ,5]
-// for (const num of arr) {
-//     console.log(num); // 1, 2, 3, undifined 4, 5
+const arr = [1, 3, ,4 ,5]
+for (const num of arr) {
+    console.log(num); // 1, 2, 3, undifined 4, 5
     
-// }
+}
 
-// const greeting = "Hello Arjun!"
-// for (const greet of greeting) {
-//     console.log(`Ecah char is ${greet}`);
-// }
+const greeting = "Hello Arjun!"
+for (const greet of greeting) {
+    console.log(`Ecah char is ${greet}`);
+}
 
-// Maps 
+Maps 
 const map = new Map() 
 map.set('IN', "india")
 map.set('USA', "United Sates of America")
 map.set('FR', "France")
 
 
-console.log(map);
+console.log(map); 
 
+Map(3) {
+    'IN' => 'india',
+    'USA' => 'United Sates of America',
+    'FR' => 'France'
+  }
+
+const myObject = {
+    gmail : "kumararjun26401@gmail.com",
+    gmail1 : "kumar@gmail.com"
+}
+
+for ( const [key, value] of myObject ) {
+    console.log(key, ":-", value);
+    
+}
