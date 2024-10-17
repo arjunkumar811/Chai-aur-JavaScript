ca81ee011e94ab0bda30fb28d295bbc0b78d3b35
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-17 09:24:36 +0000

    three.js

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -3,8 +3,14 @@
 // ["", "", ""]
 // [{}, {}, {}]
 
-const arr = [1, 3, ,4 ,5]
-for (const num of arr) {
-    console.log(num); // 1, 2, 3, undifined 4, 5
+// const arr = [1, 3, ,4 ,5]
+// for (const num of arr) {
+//     console.log(num); // 1, 2, 3, undifined 4, 5
+    
+// }
+
+const greeting = "Hello Arjun!"
+for (const greet of greeting) {
+    console.log(`Ecah char is ${greet}`);
     
 }
