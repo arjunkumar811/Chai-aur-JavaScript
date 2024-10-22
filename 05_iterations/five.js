b08249b844a2c01ffcc4ff6701438a55573d9d20
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-22 16:18:21 +0000

    five.js

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,6 +1,11 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
-coding.forEach(function (val) {
-    console.log(val);
+// coding.forEach(function (val) {
+//     console.log(val);
      
+// })
+
+coding.forEach((item) => {
+    console.log(item);
+    
 })
