75b6dd1d6b43828d49155f4576f9ef7237886b90
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-22 16:12:20 +0000

    four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -15,3 +15,14 @@ for ( const key in myObject ) {
 // swipt Shotcut is for swipt by apple
     
 } 
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
