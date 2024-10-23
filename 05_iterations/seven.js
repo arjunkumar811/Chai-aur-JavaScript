4cceb84a6b892ddcb71d0889e2fe91bd3450634d
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-23 15:23:22 +0000

    seven

---
+++ 05_iterations/seven.js
@@ -0,0 +1,11 @@
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNumbers.map((num) =>{ return num + 10 })
+
+console.log(newNums); 
+
+// [
+//     11, 12, 13, 14, 15,
+//     16, 17, 18, 19, 20
+//   ]
+ 
