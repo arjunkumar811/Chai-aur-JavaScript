a4b463a1055f64fc62c44c6f89b1d436a2c18dda
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-23 15:03:47 +0000

    six

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,6 +1,15 @@
-const coding = ["js", "ruby", "java", "python", "cpp"]
+// const coding = ["js", "ruby", "java", "python", "cpp"]
 
- const values = coding.forEach((item)=> {
-    // console.log(item);
-      return item;
+//  const values = coding.forEach((item)=> {
+//     // console.log(item);
+//       return item;
+// })
+
+
+const myName = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myName.filter((num)=> {
+    return num > 4
 })
+
+console.log(newNums); // [ 5, 6, 7, 8, 9, 10 ]
