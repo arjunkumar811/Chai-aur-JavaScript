77204645fd539e5d21b69922e5fed667e4998be9
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-23 14:56:05 +0000

    five.js

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -11,8 +11,32 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // })
 
 
-function printMe(item){
-    console.log(item);
-}
+// function printMe(item){
+//     console.log(item);
+// }
 
-coding.forEach(printMe)
+// coding.forEach(printMe)
+
+const myCoding = [
+    {
+        lanName : "javascript",
+        lanFileName : "js"
+    },
+    {
+        lanName: "Python",
+        lanFileName: "py"
+    },
+    {
+        lanName : "Java",
+        lanFileName : "java"
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.lanName);
+})
+
+
+// javascript
+// Python
+// Java
