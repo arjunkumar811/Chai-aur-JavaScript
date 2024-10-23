707ae1950b017925d323d395d350b7552af6f16c
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-23 18:33:31 +0000

    Prototype

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -4,3 +4,29 @@ let mySchool = "SBR     "
 console.log(myName.trueLength); // Undefined
 
 
+let myHeros = ["thor", "spiderman"]
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+
+Object.prototype.arjun = function(){
+    console.log(`Arjun is present in all objects`);
+    
+}
+
+Array.prototype.heyArjun = function(){
+    console.log(`Arjun says hello`);
+    
+}
+
+heroPower.arjun() // Arjun is present in all objects
+myHeros.arjun() // Arjun is present in all objects
+myHeros.heyArjun() // Arjun says hello
+// heroPower.heyArjun()
