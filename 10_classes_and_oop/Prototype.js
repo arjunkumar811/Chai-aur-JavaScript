749669e0537f3066c437f1a5648226bceb2e2c04
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-23 18:37:54 +0000

    Prototype

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -1,32 +1,57 @@
-let myName = "Arjun     "
-let mySchool = "SBR     "
+// let myName = "Arjun     "
+// let mySchool = "SBR     "
 
-console.log(myName.trueLength); // Undefined
-
-
-let myHeros = ["thor", "spiderman"]
-
-let heroPower = {
-    thor: "hammer",
-    spiderman: "sling",
-
-    getSpiderPower: function(){
-        console.log(`Spidy power is ${this.spiderman}`);
-    }
-}
+// console.log(myName.trueLength); // Undefined
 
 
-Object.prototype.arjun = function(){
-    console.log(`Arjun is present in all objects`);
+// let myHeros = ["thor", "spiderman"]
+
+// let heroPower = {
+//     thor: "hammer",
+//     spiderman: "sling",
+
+//     getSpiderPower: function(){
+//         console.log(`Spidy power is ${this.spiderman}`);
+//     }
+// }
+
+
+// Object.prototype.arjun = function(){
+//     console.log(`Arjun is present in all objects`);
     
-}
+// }
 
-Array.prototype.heyArjun = function(){
-    console.log(`Arjun says hello`);
+// Array.prototype.heyArjun = function(){
+//     console.log(`Arjun says hello`);
     
+// }
+
+// heroPower.arjun() // Arjun is present in all objects
+// myHeros.arjun() // Arjun is present in all objects
+// myHeros.heyArjun() // Arjun says hello
+// // heroPower.heyArjun()
+
+
+
+/// Inheritance
+
+const User = {
+    name: "Arjun",
+    email: "Kumararjun264912gmail.com",
 }
 
-heroPower.arjun() // Arjun is present in all objects
-myHeros.arjun() // Arjun is present in all objects
-myHeros.heyArjun() // Arjun says hello
-// heroPower.heyArjun()
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
