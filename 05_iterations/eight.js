ce55a6e63303e4514a1393bf853ecbaa12d4f68e
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-23 15:40:46 +0000

    Eight

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -3,3 +3,27 @@ const myName = [1, 2, 3]
 const myTotal = myName.reduce((acc, curr) => acc+curr, 0)
 
 console.log(myTotal); // 6
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay); //22996
