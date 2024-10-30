503baf2f9d9eb15069f33a444b800519db01c4cc
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-30 14:35:03 +0000

    nine.js

---
+++ 05_iterations/nine.js
@@ -0,0 +1,8 @@
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element == 5) { 
+        console.log("5 is best number"); // 5 is best number  
+    }
+    console.log(element); // 0 1 2 3 4 5 is best number 6 7 8 9 10
+}
