2831c17d38e43efa6ccfafc8283b68ee58a23fd6
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-30 14:35:34 +0000

    nine.js

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -6,3 +6,16 @@ for (let i = 0; i <= 10; i++) {
     }
     console.log(element); // 0 1 2 3 4 5 is best number 6 7 8 9 10
 }
+
+
+
+for (let i=1; i<=10; i++) {
+    console.log(`Outer loop value : ${i}`);
+
+    for(let j=1; j<=10; j++) {
+    console.log(`Inner loop value : ${j}`);
+      console.log(i + '*' + j + ' = ' + i*j);
+      
+    }
+}
+ 
