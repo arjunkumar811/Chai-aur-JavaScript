4993349bba9a6386513f52816bb937a0b9763318
Arjun Kumar <153661552+arjunkumar811@users.noreply.github.com>
2024-10-30 14:36:01 +0000

    nine.js

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -19,3 +19,34 @@ for (let i=1; i<=10; i++) {
     }
 }
  
+
+
+let myArray = ["yash", "druve", "rokey"]
+console.log(myArray.length); // 3
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element); // yash druve rokey
+}
+
+
+// break and continue 
+
+for (let i=1; i<=20; i++) {
+    if(i == 5) {
+        console.log(`Detected 5`); // Detected 5
+       break
+    }
+    console.log(`value is : ${i}`);
+}
+
+
+// continue
+
+for (let i=1; i<=20; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`); // Detected 5
+        continue
+    }
+    console.log(`Value of i is ${i}`);
+    
+}
